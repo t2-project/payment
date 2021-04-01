3047c5cf2e1be2b69e8e26e256306837ccf2c0b6
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2021-04-01 10:34:34 +0200

    cleaned test

---
+++ src/test/java/de/unistuttgart/t2/payment/PaymentTest.java
@@ -0,0 +1,23 @@
+package de.unistuttgart.t2.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * i dont even know what to test here...
+ * @author maumau
+ *
+ */
+@ContextConfiguration(classes = TestContext.class)
+public class PaymentTest {
+	
+	@Test
+	public void foo() {
+		
+	}
+
+}
---
+++ src/test/java/de/unistuttgart/t2/payment/TestContext.java
@@ -0,0 +1,9 @@
+package de.unistuttgart.t2.payment;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestContext {
+
+}
