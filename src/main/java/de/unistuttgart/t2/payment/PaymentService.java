734a1cbc75418cc171592c14e51fcb384992e13a
Sarah Sophie Stieß <st115875@stud.uni-stuttgart.de>
2021-04-15 17:52:38 +0200

    payment does no rely on provider to execute the payment.

--- src/main/java/de/unistuttgart/t2/payment/PaymentService.java
+++ src/main/java/de/unistuttgart/t2/payment/PaymentService.java
@@ -1,35 +1,37 @@
 package de.unistuttgart.t2.payment;
 
-
-import java.util.Random;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
 
+import de.unistuttgart.t2.payment.provider.PaymentData;
 import de.unistuttgart.t2.payment.saga.CreditCardInfo;
 
 public class PaymentService {
 	
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
 	
+	@Value("${payment.provider.dummy.url}")
+	String providerUrl;
+	
+	@Autowired 
+	RestTemplate template;
+	
 	/**
-	 * executes the payment.
+	 * contact some payment provider to execute the payment. 
 	 * 
-	 * always fails on with checksum "bad" and always succeeds with checksum "good".
-	 * if checksum is neither if randomly fails.
+	 * the call might either timeout, or the payment itself might fail, or it is successful.
 	 * 
-	 * @param card information about the creditcard
+	 * @param card information about the credit card
 	 * @param total money to be payed
-	 * @throws Exception if payment failed
 	 */
-	public void handleSagaAction(CreditCardInfo card, double total) throws Exception{
-		if (card.getChecksum().equals("bad")) 
-			throw new Exception("forced failure");
-		if (card.getChecksum().equals("good"))
+	public void handleSagaAction(CreditCardInfo card, double total) {
+		if (card.getChecksum().equals("tolazytostartprovider")) {
 			return;
-		
-		if (new Random().nextInt(10) == 0) {
-			throw new Exception("random failure");
 		}
+		LOG.info("post to " + providerUrl);
+		template.postForObject(providerUrl, new PaymentData(card.getCardNumber(), card.getCardOwner(), card.getChecksum(), total), Void.class);
 	}
 }
