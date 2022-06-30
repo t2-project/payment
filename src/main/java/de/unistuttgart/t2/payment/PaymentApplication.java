70ec59dd24112a16c7149df573131a376ea39093
delvh <leon@kske.dev>
2022-06-30 19:38:53 +0200

    Add autoscaling management to payment

--- src/main/java/de/unistuttgart/t2/payment/PaymentApplication.java
+++ src/main/java/de/unistuttgart/t2/payment/PaymentApplication.java
@@ -4,22 +4,19 @@ import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import de.unistuttgart.t2.payment.config.ExcludeSagaConfig;
-import de.unistuttgart.t2.payment.config.IncludeSagaConfig;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
+import de.unistuttgart.t2.common.BaseScan;
+import de.unistuttgart.t2.payment.config.*;
+import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
 
 @Import({ IncludeSagaConfig.class, ExcludeSagaConfig.class })
 @EnableAutoConfiguration
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = BaseScan.class)
 public class PaymentApplication {
 
     @Value("${t2.payment.provider.timeout:10}")
