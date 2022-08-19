735ca3186331e1533d2a109a3d34ebbe6d2edd5a
delvh <leon@kske.dev>
2022-08-19 15:48:26 +0200

    Remove redundant class scan and annotation

--- src/main/java/de/unistuttgart/t2/payment/PaymentApplication.java
+++ src/main/java/de/unistuttgart/t2/payment/PaymentApplication.java
@@ -4,19 +4,17 @@ import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import de.unistuttgart.t2.common.BaseScan;
 import de.unistuttgart.t2.payment.config.*;
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
 
 @Import({ IncludeSagaConfig.class, ExcludeSagaConfig.class })
-@EnableAutoConfiguration
-@SpringBootApplication(scanBasePackageClasses = { BaseScan.class, PaymentApplication.class })
+@SpringBootApplication
 public class PaymentApplication {
 
     @Value("${t2.payment.provider.timeout:10}")
